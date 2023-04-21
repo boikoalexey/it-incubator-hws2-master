d5f85d57f852b734335091f370aaec031a4a3815
Alexey Boyko <alexeyboiko@bk.ru>
2023-04-21 19:09:57 +0300

    HW 4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
+        onChange && onChange(e) // вызываем onChange, если он есть
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
 
     }
 
